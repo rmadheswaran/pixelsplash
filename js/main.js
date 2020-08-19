289a71ed5af0304e8297f06f97ab2cefc4b16dd7
Madeshwaran <madesh@baf.com>
2020-08-19 23:29:36 +0530

    index modified

--- js/main.js
+++ js/main.js
@@ -2,7 +2,15 @@ window.onload = () => {
   'use strict';
 
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./sw.js');
-  }
+  navigator.serviceWorker.register('sw.js');
 }
+
+document.querySelector('#show').addEventListener('click', () => {
+  const iconUrl = document.querySelector('select').selectedOptions[0].value;
+  let imgElement = document.createElement('img');
+  imgElement.src = iconUrl;
+  document.querySelector('#container').appendChild(imgElement);
+});
+}
+
+
