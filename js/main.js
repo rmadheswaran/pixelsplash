91c290038481f9c218518fa9d376009320615de8
rmadheswaran <34329467+rmadheswaran@users.noreply.github.com>
2020-08-20 11:52:59 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,12 +5,7 @@ window.onload = () => {
   navigator.serviceWorker.register('sw.js');
 }
 
-document.querySelector('#show').addEventListener('click', () => {
-  const iconUrl = document.querySelector('select').selectedOptions[0].value;
-  let imgElement = document.createElement('img');
-  imgElement.src = iconUrl;
-  document.querySelector('#container').appendChild(imgElement);
-});
+
 }
 
 
